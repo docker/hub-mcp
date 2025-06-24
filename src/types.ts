7c41888e58064f769832645428de73058639d141
Luca Druda <lucadruda@gmail.com>
2025-06-24 20:25:18 +0200

    fix comment

--- src/types.ts
+++ src/types.ts
@@ -26,26 +26,7 @@ export function createPaginatedResponseSchema<ItemType extends z.ZodTypeAny>(
       previous: z.string().nullable().optional(),
       results: z.array(itemSchema),
     })
-    // .optional(); // optional because the response can be empty. This is a workaround for https://github.com/modelcontextprotocol/typescript-sdk/issues/654
+    // Currently we can't have optional object as a schema because structured content is always evaluated even when an error occurs.
+    // See https://github.com/modelcontextprotocol/typescript-sdk/issues/654
 }
 
-export type Organization = {
-  id: string;
-  uuid: string;
-  orgname: string;
-  full_name: string;
-  location: string;
-  company: string;
-  profile_url: string;
-  date_joined: string;
-  gravatar_url: string;
-  gravatar_email: string;
-  type: string;
-  badge: string;
-  is_active: boolean;
-  user_role: string;
-  user_groups: string[];
-  org_groups_count: number;
-  plan_name: string;
-  parent_name: string;
-};
